5ce0e928a1cf6fa6bb94b309b367ea91a2606c48
Damian Colaneri <dcolaneri@gmail.com>
2021-03-14 18:21:17 -0300

    [ADD] animations

--- faq-accordion-card-main/main.js
+++ faq-accordion-card-main/main.js
@@ -2,6 +2,6 @@ let element = document.querySelectorAll('li');
 element.forEach(function (element) {
 	element.addEventListener('click', function (e) {
 		let question = e.currentTarget.parentElement;
-		question.classList.toggle('show-text');
+		question.classList.toggle('show__text');
 	});
 });
