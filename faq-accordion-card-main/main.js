7f69b70af4e8355c7268cc99c86ca7bd47dfef19
Damian Colaneri <dcolaneri@gmail.com>
2021-03-14 19:42:45 -0300

    [FIX] id error

--- faq-accordion-card-main/main.js
+++ faq-accordion-card-main/main.js
@@ -1,4 +1,4 @@
-let element = document.querySelectorAll('li');
+let element = document.querySelectorAll('.question__title');
 element.forEach((element) => {
 	element.addEventListener('click', (e) => {
 		let question = e.currentTarget.parentElement;
