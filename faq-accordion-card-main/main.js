c149551b4e9eaaf95f3400a4ed8c0c3c49e14964
Damian Colaneri <dcolaneri@gmail.com>
2021-03-14 16:55:41 -0300

    first commit

---
+++ faq-accordion-card-main/main.js
@@ -0,0 +1,7 @@
+let element = document.querySelectorAll('li');
+element.forEach(function (element) {
+	element.addEventListener('click', function (e) {
+		let question = e.currentTarget.parentElement;
+		question.classList.toggle('show-text');
+	});
+});
